765aa761feffa4a70adaa1668057cb719a1f31f3
pseoane <pjseoane@gmail.com>
2018-11-09 11:30:30 -0300

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	np test.ipynb

---
+++ optionModelsCYTHON/JRCython.py
@@ -0,0 +1,6 @@
+import pyximport, numpy
+pyximport.install(setup_args=dict(include_dirs=[numpy.get_include()]))
+
+from optionModelsCYTHON import jarrowCython as jrcy
+a=jrcy.jarrowRuddCy("S",100,100,365,0.3,0.03,-1,0,True,100,6,0)
+print(a)
